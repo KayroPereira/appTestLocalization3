92d1c87953c712834e133502887715ceeaa416c5
Kayro Pereira <kayrofellyxbr@gmail.com.br>
2020-05-14 18:23:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/apptestlocalization3/MainActivity.java
@@ -0,0 +1,343 @@
+package com.example.apptestlocalization3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.icu.text.SimpleDateFormat;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.SuccessContinuation;
+import com.google.android.gms.tasks.Task;
+
+/*
+caixa
+57 até a parte superior da saida
+62 até a parte inferior da saida
+8 até o sensor
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    final int REQUEST_CHECK_SETTINGS = 10;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+
+    private TextView tvGeral;
+    private Button btUpdate;
+
+    private boolean requestingLocationUpdates = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvGeral = (TextView) findViewById(R.id.tvGeral);
+        btUpdate = (Button) findViewById(R.id.btUpdate);
+
+        pedirPermissoes();
+
+        createLocationRequest();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        //chamado quando a activity é inicializada e a localização está ativa
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+                Toast.makeText(getApplicationContext(), "Autorização ok", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //chamado quando há falha no pedido de localização
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "teste0", Toast.LENGTH_LONG).show();
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    Toast.makeText(getApplicationContext(), "teste1", Toast.LENGTH_LONG).show();
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                        Toast.makeText(getApplicationContext(), "Autorização negada", Toast.LENGTH_LONG).show();
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        Toast.makeText(getApplicationContext(), "Erro Autorização", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
+        //chamado quando se verifica o resultado da última localização indepedentimente da conexão está ativada ou não
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                Toast.makeText(getApplicationContext(), "teste-10", Toast.LENGTH_LONG).show();
+                if (location != null) {
+                    tvGeral.setText("Latitude: " + location.getLatitude() + "\nLogitude: " + location.getLongitude() + "\nProvider: " + location.getProvider()
+                            + "\nTime: " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(location.getTime()) + "\nAccuracy: " + location.getAccuracy()
+                            + "\nAltitude: " + location.getAltitude() + "\nSpeed: " + location.getSpeed());
+                } else {
+                        tvGeral.setText("Erro: location null");
+                }
+            }
+        });
+
+            locationCallback = new LocationCallback() {
+            @Override
+            //chamado quando a conexão está ativada e se tem um resultado
+            public void onLocationResult(LocationResult locationResult) {
+                Toast.makeText(getApplicationContext(), "teste-11", Toast.LENGTH_LONG).show();
+                if (locationResult == null) {
+                    tvGeral.setText("Erro: location null");
+                    return;
+                }
+
+                for (Location location : locationResult.getLocations()) {
+                    tvGeral.setText("Latitude: " + location.getLatitude() + "\nLogitude: " + location.getLongitude() + "\nProvider: " + location.getProvider()
+                            + "\nTime: " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(location.getTime()) + "\nAccuracy: " + location.getAccuracy()
+                            + "\nAltitude: " + location.getAltitude() + "\nSpeed: " + location.getSpeed());
+                }
+            }
+
+            //Chamado quando há uma alteração na disponibilidade dos dados do local.
+            @Override
+            public void onLocationAvailability(LocationAvailability avail) {
+                super.onLocationAvailability(avail);
+
+                //testa se a localização esta habilitada
+                if (avail.isLocationAvailable())
+                    Toast.makeText(getApplicationContext(), "teste-2", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getApplicationContext(), "teste-3", Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+
+    //chamado quando se tem um resposta da solicitação de autorização da activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(getApplicationContext(), "teste-12", Toast.LENGTH_LONG).show();
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getApplicationContext(), "teste5", Toast.LENGTH_LONG).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getApplicationContext(), "teste6", Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "teste7", Toast.LENGTH_LONG).show();
+                        break;
+                }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!requestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    private void startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        requestingLocationUpdates = true;
+    }
+
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+        requestingLocationUpdates = false;
+    }
+
+    protected void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(5000);
+        //locationRequest.setFastestInterval(15000);
+        //locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //verificar a possibilidade de conexão por outras redes
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    }
+
+    private void pedirPermissoes() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CHECK_SETTINGS);
+        }
+        else {
+            configurarServico();
+            createLocationRequest();
+        }
+    }
+
+    public void configurarServico(){
+        try {
+            Toast.makeText(this, "configurarServico", Toast.LENGTH_LONG).show();
+            //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 5, locationListener, null);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener, null);
+            //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener, null);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (android.location.LocationListener) locationListener, null);
+        }catch(SecurityException ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+/*
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+        btUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pedirPermissoes();
+            }
+        });
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                atualizar(location);
+                //locationListener.onProviderDisabled("");                    //remover caso queira deixar atualização automática
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+                Toast.makeText(getApplicationContext(), "onStatusChanged", Toast.LENGTH_LONG).show();
+            }
+
+            public void onProviderEnabled(String provider) {
+                Toast.makeText(getApplicationContext(), "onProviderEnabled", Toast.LENGTH_LONG).show();
+            }
+
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(getApplicationContext(), "onProviderDisabled", Toast.LENGTH_LONG).show();
+                locationManager.removeUpdates(locationListener);
+            }
+        };
+    }
+
+    private void pedirPermissoes() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1); //definir um requestCode
+        }
+        else
+            configurarServico();
+    }
+
+    public void configurarServico(){
+        try {
+            Toast.makeText(this, "getGPS", Toast.LENGTH_LONG).show();
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 5, locationListener, null);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener, null);
+            //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener, null);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (android.location.LocationListener) locationListener, null);
+        }catch(SecurityException ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void atualizar(Location location){
+        try {
+            tvGeral.setText("Latitude: " + location.getLatitude() + "\nLogitude: " + location.getLongitude() + "\nProvider: " + location.getProvider()
+                    + "\nTime: " + new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(location.getTime()) + "\nAccuracy: " + location.getAccuracy()
+                    + "\nAltitude: " + location.getAltitude() + "\nSpeed: " + location.getSpeed());
+        } catch (Exception e) {
+            tvGeral.setText("Erro: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        Toast.makeText(this, "Não vai funcionar!!!", Toast.LENGTH_LONG).show();
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    configurarServico();
+                } else {
+                    Toast.makeText(this, "Não vai funcionar!!!", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+}
+
+ */
